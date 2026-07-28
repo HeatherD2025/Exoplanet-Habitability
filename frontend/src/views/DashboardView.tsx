e649b059ffe34d0df4a39a87510fd74adabfd9ae
heatherd2025 <heatherdeliso@gmail.com>
2026-07-27 20:45:52 -0400

    margin increase on planet filters for browse section 3

--- frontend/src/views/DashboardView.tsx
+++ frontend/src/views/DashboardView.tsx
@@ -56,7 +56,7 @@ export default function DashboardView({ planets }: DashboardViewProps) {
   });
 
   return (
-    <Container className="planet-filter-and-grid-container m-2 p-2">
+    <Container className="planet-filter-and-grid-container m-3 p-2">
       <PlanetFilters onFilterChange={setActiveFilters} />
 
       {/* Grid response output layout */}
