58d66ba51932115fdbb970348fe1e509211f1381
heatherd2025 <heatherdeliso@gmail.com>
2026-08-12 16:03:25 -0400

    spacing work on planet card grid

--- frontend/src/components/PlanetDetailModal.tsx
+++ frontend/src/components/PlanetDetailModal.tsx
@@ -93,7 +93,7 @@ export default function PlanetDetailModal({
           <br></br>
           <div>{formatOrbitalDistance(orbitalDistance)}</div>
           <br></br>
-          <div className="p-3 border border-secondary rounded">
+          <div className="p-1 border border-secondary rounded">
             <p className="text-uppercase fs-7 mb-1">Listing date: {planet?.discoveryYear || "Unknown"}</p>
           </div>
         </Modal.Body>
